dd1b8c8b2e2232c2799df125b082a478026b9fe4
root <root@deploy-1.ops>
2018-12-20 20:22:02 +0000

    creating ec2/s-groups/running remote cmds/ssh

---
+++ ec2.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "ec2-create-users" {
+  ami           = "${lookup(var.amis, var.region)}"
+  instance_type = "t2.micro"
+  subnet_id     = "${var.subnet_id}"
+  key_name      = "${var.key_pair_name}"
+
+  tags {
+    Name = "${var.instance_name}"
+  }
+}
+ 
+resource "aws_security_group" "example" {
+  name   = "${var.instance_name}"
+  vpc_id = "${var.vpc_id}" 
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = "${var.ssh_port}"
+    to_port   = "${var.ssh_port}"
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = "${aws_instance.ec2-create-users.id}"
+}
+
+resource "null_resource" "everything_provisioner" {
+  triggers {
+    public_ip  = "${aws_instance.ec2-create-users.public_ip}"
+    private_ip = "${aws_instance.ec2-create-users.private_ip}"
+  }
+
+  connection {
+    type = "ssh"
+    host = "${aws_instance.ec2-create-users.private_ip}"
+    user = "${var.ssh_user}"
+    port = "${var.ssh_port}"
+    agent = true
+  }
+ 
+  provisioner "file" {
+    source      = "files/example-file.txt"
+    destination = "/tmp/example-file.txt"
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+      "echo 'this-is-2-now' >> /tmp/example-file.txt",
+      "cat /tmp/example-file.txt",
+      "echo /tmp/example-file.txt > /tmp/new-example-file.txt"
+    ]   
+  }
+
+  provisioner "local-exec" {
+    command = "scp -i /root/.ssh/terraform.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${var.ssh_user}@${aws_instance.ec2-create-users.public_ip}:/tmp/new-example-file.txt new-example-file.txt"
+  }
+
+  depends_on = ["aws_eip.ip"]
+}
--- var.tf
+++ var.tf
@@ -1,6 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
 variable "subnet_id" {}
+variable "vpc_id" {}
 
 variable "region" {
   default = "us-east-1"
@@ -8,4 +9,21 @@ variable "region" {
 
 variable "amis" {
   type = "map"
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+  } 
+}
+
+variable "key_pair_name" {
+  default = "terraform"
+}
+variable "instance_name" {
+  default = "ec2-with-users"
+}
+variable "ssh_port" {
+  default = 22
+}
+variable "ssh_user" {
+  default = "ubuntu"
 }
